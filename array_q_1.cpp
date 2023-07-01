3a2e23220ef9d72a1001bd776ce168c2ad136f8c
Dinky kumawat <97829597+kumawatdinky1@users.noreply.github.com>
2023-07-01 19:31:39 +0530

    Create array_q_1.cpp

---
+++ array_q_1.cpp
@@ -0,0 +1,11 @@
+//Largest Element in the Array
+#include <bits/stdc++.h> 
+int largestElement(vector<int> &arr, int n) {
+    int largest=arr[0];
+    for(int i=1;i<n;i++)
+    {
+      if(arr[i]>largest)
+      largest=arr[i];
+    }
+    return largest;
+}
