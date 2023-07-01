04eee42d6d73b1a64fb940d7f520311491185524
Dinky kumawat <97829597+kumawatdinky1@users.noreply.github.com>
2023-07-01 19:38:14 +0530

    Create Array_q_4.cpp

---
+++ Array_q_4.cpp
@@ -0,0 +1,16 @@
+//Remove Duplicates from Sorted Array
+
+int removeDuplicates(vector<int> &arr, int n) {
+	// Write your code here.
+	int i=0;
+	for(int j=1;j<n;j++)
+	{
+		if(arr[j]!=arr[i])
+		{
+			arr[i+1]=arr[j];
+			i++;
+		}
+		
+	}
+	return i+1;
+}
