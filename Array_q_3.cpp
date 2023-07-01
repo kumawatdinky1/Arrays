175a320dd170111f2852116918785d4c596a3854
Dinky kumawat <97829597+kumawatdinky1@users.noreply.github.com>
2023-07-01 19:35:36 +0530

    Create Array_q_3.cpp

---
+++ Array_q_3.cpp
@@ -0,0 +1,16 @@
+//Ninja And The Sorted Check
+
+int isSorted(int n, vector<int> a) {
+    // Write your code here.
+    for(int i=1;i<n;i++)
+     {
+         if(a[i]>=a[i-1])
+         {
+             
+         }
+         else{
+             return false;
+         }
+     }
+     return true;
+}
